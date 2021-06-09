efaa6fe2c9ce5fd932f3d57b7a66cbf809278e30
mbrsagor <mbrsagor@gmail.com>
2021-06-09 09:00:28 +0600

    New: Encapsulation basic example

---
+++ encapsulation/EncapsulationDemo.java
@@ -0,0 +1,36 @@
+class GetterSetterClass {
+
+    private String name;
+    private int age;
+
+    // Here, getter and setter implementations
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int setAge(int new_age) {
+        age = new_age;
+        return age;
+    }
+
+    public String getName(String new_name) {
+        name = new_name;
+        return name;
+    }
+
+}
+
+
+class EncapsulationDemo {
+    public void main(String[] args){
+        GetterSetterClass  getterSetter = new GetterSetterClass();
+        getterSetter.setAge(26);
+        getterSetter.setName("Bozlur Rosid Sagor");
+
+        System.out.print("Name : " + getterSetter.getName()) + " Age : " + getterSetter.getAge()));
+    }
+}
